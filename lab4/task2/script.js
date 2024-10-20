d0e67a5d1a70e05ad348d9d45afc4b321416e13f
Oleksandr Borovyk <maingroon@gmail.com>
2024-10-20 20:44:18 +0300

    lab 4

--- lab4/task2/script.js
+++ lab4/task2/script.js
@@ -17,3 +17,23 @@ document.getElementById('email').addEventListener('blur', async () => {
         emailStatusDiv.style.color = 'red';
     }
 });
+
+document.getElementById('password').addEventListener('blur', async () => {
+    const password = document.getElementById('password').value;
+    const passwordStatusDiv = document.getElementById('passwordStatus');
+
+    passwordStatusDiv.textContent = 'Checking password...';
+
+    try {
+        // Імітація асинхронного запиту до сервера для перевірки пароля
+        const isSecure = await new Promise(resolve => setTimeout(() => {
+            resolve(password !== 'password');
+        }, 1000));
+
+        passwordStatusDiv.textContent = isSecure ? 'Password is secure' : 'Please choose a more secure password';
+        passwordStatusDiv.style.color = isSecure ? 'green' : 'red';
+    } catch (error) {
+        passwordStatusDiv.textContent = 'Error checking password.';
+        passwordStatusDiv.style.color = 'red';
+    }
+});
